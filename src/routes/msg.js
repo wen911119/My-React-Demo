78a6a63f48ce7d0e9cd970f8b29b38573d26cf05
wenjun <771299231@qq.com>
2016-11-15 17:58:24 +0800

    增加了webpack异步加载的注释

--- src/routes/msg.js
+++ src/routes/msg.js
@@ -13,6 +13,12 @@ export default {
   /* 布局基页 */
   // https://segmentfault.com/a/1190000007141049
   // react-router 按需加载
+  // 按需加载之后，我们需要让路由动态加载组件，需要将 component 换成 getComponent
+  // require.ensure(dependencies, callback, chunkName)
+  // 这是 webpack 提供的方法，这也是按需加载的核心方法。第一个参数是依赖，第二个是回调函数，第三个就是上面提到的 chunkName，用来指定这个 chunk file 的 name。
+  // 如果需要返回多个子组件，则使用 getComponents 方法，将多个组件作为一个对象的属性通过 cb 返回出去即可。这个在官方示例也有，但是我们这里并不需要，
+  // 而且根组件是不能返回多个子组件的，所以使用 getComponent。
+  
   getComponent (nextState, cb) {
     require.ensure([], (require) => {
       // 立即注入 Reducer
