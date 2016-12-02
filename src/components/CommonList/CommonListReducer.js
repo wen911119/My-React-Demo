7649b6b4450dc253f9b654adcd149cacf65c859c
wenjun <771299231@qq.com>
2016-12-02 18:03:10 +0800

    继续通用列表开发

--- src/components/CommonList/CommonListReducer.js
+++ src/components/CommonList/CommonListReducer.js
@@ -1,3 +1,31 @@
 /**
  * Created by wen91 on 2016/12/2.
  */
+import createReducer from 'UTIL/createReducer'
+
+
+const initState = {
+    pages: [    // 列表每页的数据
+        {
+            pageContent: [], // 该页的实际内容，具体格式和子组件相关，这里没有限制
+            pageNum: 1,   // 页码
+            isShow: true, // 正常展示还是回收
+            height: '800px', // 该页的高度，用于回收状态占位
+
+        }
+    ],
+    page: 1,    // 当前请求页
+    rows: 10,   // 每页几条数据
+    pageNum: 0, // 总页数
+    currentPageView: 0, // 当前可视区内是第几页
+    needLoading: true, // 是否需要加载数据
+    dataFormatter: (originData) => originData  // 格式化方法
+}
+
+const ACTION_HANDLERS = {
+    fetchListData: (listState, {payload}) => {
+
+    }
+}
+
+export default createReducer(initState.list, ACTION_HANDLERS)
---
+++ src/components/CommonList/ListItems/GoodsListItem.js
@@ -0,0 +1,24 @@
+/**
+ * Created by MB-WJ on 2016/12/2.
+ */
+import React, {Component} from 'react'
+
+export default class GoodsListItem extends Component {
+    render() {
+        let {productName, imgUrl_300_300: mgUrl, brandName, salesPrice} = this.props.data
+
+        return (
+            <div className="flex-box listItem" style={{height: '360px'}}>
+                <div className="goods_pic">
+                    <img src={mgUrl} alt=""/>
+                </div>
+                <div className="goods_text flex-item-1">
+                    <div className="goods_brand">{brandName}</div>
+                    <div className="goods_name">{productName}</div>
+                    <div className="goods_price">{salesPrice}</div>
+                </div>
+            </div>
+        )
+
+    }
+}
